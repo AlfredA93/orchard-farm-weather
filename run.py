5faae1fa044044e89c84945a4ed6aeee9cc44a3a
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-07-03 11:08:13 +0200

    Delete run.py file. Rename test.py to run.py

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
-import gspread
 import plotext
 from datetime import datetime
+import pandas as pd
+import gspread
 from google.oauth2.service_account import Credentials
+import blessed
+import time
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -17,24 +17,63 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('orchard_farm_weather_data')
 
+DF = pd.DataFrame(SHEET.worksheet('data').get_all_records())
+
+TERM = blessed.Terminal()
+
+
 def new_date(new_row):
     """
     User input today's date function.
     """
+    print(TERM.clear)
+    print("Welcome to Orchard Farm Weather Data Collection.")
+    print("")
     while True:
         print("Please enter the date today")
         print("Format: YYYY-MM-DD")
         date_str = input("Enter the date here:\n")
-        if date_str != datetime.today().date().strftime('%Y-%m-%d'):    # Is user date entry today?
-            print("Error - incorrect date entered. Restarting...\n")
-            new_date(new_row)                                           # Go back to beginning if date incorrect
+        if date_str != datetime.today().date().strftime('%Y-%m-%d'):
+            print("Error - incorrect date entered. Restarting...")
+            time.sleep(1)
+            print("...")
+            time.sleep(1)
+            print("...")
+            time.sleep(0.5)
+            new_date(new_row)  # Go back to beginning if date incorrect
             break
-        year_num = int(datetime.today().date().strftime('%Y'))          # Create year number
-        day_num_of_year = int(datetime.now().strftime('%j'))            # Create day of year
-        new_row.append(year_num)                                        # Add year number to new row data list
-        new_row.append(day_num_of_year)                                 # Add day of year to new row data list
+        year_num = int(datetime.today().date().strftime('%Y'))  # Year number
+        day_num_of_year = int(datetime.now().strftime('%j'))  # Day of year
+        new_row.append(year_num)  # Add year number to new row data list
+        new_row.append(day_num_of_year)  # Add day of year to new row data list
         return new_row
-    
+
+
+def check_date(choice):
+    day_of_year = int(datetime.now().strftime('%j'))
+    year = int(datetime.now().strftime('%Y'))
+    todays_entries = DF.loc[(DF['DOY'] == day_of_year) & (DF['YEAR'] == year)]
+    if todays_entries.empty:
+        pass
+    else:
+        print("It seems as if there is alredy an entry for today")
+        print("")
+        print(todays_entries)
+        print("")
+        print("We only keep one line of data per day in our records.")
+        print("Do you want to delete the existing entry and start a new one?")
+        delete_row = input("Type 'yes' or 'no'.\n")
+        if delete_row.lower() == "yes":
+            DF.drop(DF.index[-1], inplace=True)  # Credit 1 - See README.md
+        elif delete_row.lower() == 'no':
+            thank_you()
+            choice = "no"
+            return choice
+        else:
+            print("Error... input wasn't 'yes' or 'no'. Try again.")
+            check_date(choice)
+
+
 def new_weather(new_row, temp, range1, range2, record_num):
     """
     Master function for 3 user inputs
@@ -42,6 +81,8 @@ def new_weather(new_row, temp, range1, range2, record_num):
     - Lowest temperature today
     - Highest temperature today
     """
+    print(TERM.clear)
+    print("Orchard Farm Weather Data Collection.")
     print("")
     while True:
         print(f"{temp} today.")
@@ -50,204 +91,151 @@ def new_weather(new_row, temp, range1, range2, record_num):
             user_data = int(input(f"Enter the {temp} here:\n"))
         except ValueError:
             print("That wasn't a whole number. Please enter a whole number")
-            user_data = int(input(f"Enter the {temp} here:\n"))
-            # If user doesn't enter a number, throws error.
             continue
         else:
-            while user_data not in range(range1, range2):               # Checks for excessive input value beyond expected amount.
-                print(f"You typed {user_data} this seems unusual: Current UK record = {record_num}")
+            while user_data not in range(range1, range2):
+                print(f"A number not in usual range or not integer was typed.")
+                print(f"Current UK record = {record_num}")
                 print(f'Please try again.')
                 try:
                     user_data = int(input(f"Enter the {temp} here:\n"))
                 except ValueError:
-                    print("That wasn't a whole number. Please enter a whole number") 
-                    user_data = int(input(f"Enter the {temp} here:\n"))
-            new_row.append(user_data)                                   # Add new data to new row data list               
+                    print("Error...")
+            new_row.append(user_data)  # Add new data to new row data list
             break
     return new_row
 
-def check_inputs(new_row):
+
+def check_inputs(new_row, dont_send):
     """
     Check with user that they want to send inputs to spreadsheet
     """
+    print(TERM.clear)
+    print("Orchard Farm Weather Data Collection.")
+    print("")
+
+    low_temp = new_row[3]
+    high_temp = new_row[4]
+
+    if low_temp > high_temp:
+        print("We've noticed that the low temp is bigger than the high temp.")
+        print("So we've swapped these values for you!")
+        new_row[3] = high_temp
+        new_row[4] = low_temp
+
     user_input_checks = {
-        "Date" : datetime.today().date().strftime('%Y-%m-%d'),
-        "Rainfall (mm)" : new_row[2],
-        "Lowest Temperature (°C)" : new_row[3],
-        "Highest Temperature (°C)" : new_row[4]
+        "Date": datetime.today().date().strftime('%Y-%m-%d'),
+        "Rainfall (mm)": new_row[2],
+        "Lowest Temperature (°C)": new_row[3],
+        "Highest Temperature (°C)": new_row[4]
     }
-        
-    print("\n")
-    print("Would you like the following values to be added to the spreadsheet?\n")
-    #print(f"\n {user_input_checks}")
+
+    print("Would you like these values to be added to the spreadsheet?\n")
     for keys, values in user_input_checks.items():
-        print(keys,':', values)
-    send_inputs = input("Please type 'yes' to send and 'no' to restart programme\n")
+        print(keys, ':', values)
+    print("")
+    send_inputs = input("Please type 'yes' to send and 'no' to finish.\n")
     if send_inputs.lower() == "yes":
         send_new_row(new_row)
     elif send_inputs.lower() == 'no':
-        print("Restarting...")
-        main()
+        dont_send = "no"
+        return dont_send
     else:
         print("Error... input wasn't 'yes' or 'no'. Try again.")
         check_inputs(new_row)
 
-def duplicate_find():
-    """
-    Function to get all weather data for the same day of year. This brings back a list of cell and row numbers.
-    The function then converts to string. Strips it back into the row number.
-    It then retrieves whole row data for the matching cells.
-    It then prints out the weather data to the user.
-    """
-    print("Loading... (may take upto 30 seconds..)\n")
-    day_of_year = datetime.now().strftime('%j') 
-    duplicates = SHEET.worksheet('data').findall(f"{day_of_year}")
-    
-    row_summary = []
-    for row in range(len(duplicates)):
-        string = str(duplicates[row])
-        string_parts = string.split(" ")
-        string_row = string_parts[1]
-        string_row_num = string_row[1:]
-        string_row_2 = string_row_num.split("C")
-        row_num = string_row_2[0]
-        all_row_values = SHEET.worksheet('data').row_values(row_num)
-        row_summary.append(all_row_values)     
-        
-    #print(row_summary)
-    # historical_data = {}
-    # keys_list = []
-    # values_list = []
-    # for data in range(len(row_summary)):
-    #     first_row = row_summary[data]
-    #     keys = first_row[0]
-    #     keys_list.append(keys)
-    #     values = first_row[2:5]
-    #     values_list.append(values)
-    # print(keys_list)
-    # print(values_list)
-    
-    print("Table of Weather Data since 1993:")
-    for data in range(len(row_summary)):
-        print(f"Year: {row_summary[data][0]}. Rainfall: {row_summary[data][2]}mm Min Temp: {row_summary[data][3]}°C Max Temp: {row_summary[data][4]}°C")
-    print("\n")
 
 def send_new_row(new_row):
     """
-    Send list of user input data to spreadsheet   
+    Send list of user input data to spreadsheet
     """
-    print("Sending data to spreadsheet...")
-    worksheet_to_change = SHEET.worksheet("data")
-    worksheet_to_change.append_row(new_row)
-    print("Successfully sent.\n")
-    
+    DF.loc[len(DF)] = new_row  # Credit: Code from sparkbyexmaples.com
+    print("Sending data to spreadsheet.")
+    DF_col_val = DF.columns.values
+    DF_val = DF.values
+    SHEET.worksheet('data').update([DF_col_val.tolist()] + DF_val.tolist())
+    print("Data successfully added to spreadsheet.")
+    time.sleep(1.5)
+    print(TERM.clear)
+
+
+def chart_question(weather_type):
+    print("")
+    print(f"Shall we see a chart for {weather_type} on this day since 1993?\n")
+    chart_answer = input("Please type 'yes' to see chart and 'no' continue.\n")
+    while chart_answer.lower() not in ("yes", "no"):
+        chart_answer = input("Error... input wasn't 'yes' or 'no'. Try again.")
+    if chart_answer.lower() == "yes":
+        find_rows(weather_type)
+
+
+def find_rows(weather_type):
+    """
+    This function retrieves row information from the dataframe.
+    It splits it into the variables we'd like to show on the chart.
+    Rainfall, minimum and maximum temperatures.
+    It then shows a different chart depending on the weather_type entered.
+    """
+    print(TERM.clear)
+    print("Orchard Farm Weather Data Collection.")
+    print("")
+
+    day_of_year = int(datetime.now().strftime('%j'))
+    all_rows = DF.loc[DF['DOY'] == day_of_year]
+    yrs = all_rows["YEAR"].tolist()
+    rainfall = all_rows["RAINFALL"].tolist()
+    min = all_rows["TEMP_MIN"].tolist()
+    max = all_rows["TEMP_MAX"].tolist()
+
+    plotext.title(f"Bar Chart of {weather_type.title()} since 1993")
+    plotext.xlabel("Year\n")
+    plotext.plotsize(120, 40)
+
+    if weather_type == "rainfall":
+        plotext.bar(yrs, rainfall, width=2/5)
+        plotext.ylabel("Rainfall in mm")
+
+    elif weather_type == "contrasting temperatures":
+        plotext.multiple_bar(yrs, [max, min], label=["max", "min"], width=1/5)
+        plotext.ylabel("Temperature in °C")
+        plotext.xaxes(50)
+
+    plotext.show()
+    plotext.clear_data()
+
+
 def thank_you():
-    print("Thank you for collecting data with Orchard Farm Weather Data Collection.")
-    print("This will help with all future crop plans alongwith the understanding of climate change in our area.\n")
-    
+    """
+    Thank you section
+    """
+    print("Thank you for using Orchard Farm Weather Data Collection.")
+    print("This will help with all future crop plans.")
+    print("Alongwith understanding the effects of climate change in our area.")
+
+
 def main():
     """
     Runs all program functions in correct order
     """
-    new_row = []                                                          # List for new row
-    new_date(new_row)                                                     # Today's date input
-    new_weather(new_row, "Rainfall in millimeters", 0, 450, "341.4mm")             # Rainfall input
-    new_weather(new_row, "Lowest temperature in °C", -40, 50, "-27.4°C")  # Min temp input
-    new_weather(new_row, "Highest temperature in °C", -40, 50, "40.3°C")  # Max temp input
-    check_inputs(new_row)
-    #send_new_row(new_row)
-    duplicate_find()
-    thank_you()
-    
-print("Welcome to Orchard Farm Weather Data Collection.")
+    new_row = []  # List for new row
+    choice = ""  # Variable for check_date
+    dont_send = ""  # Variable for check_input
+    new_date(new_row)
+    if check_date(choice) == "no":
+        pass
+    else:
+        new_weather(new_row, "Rainfall in millimeters", 0, 450, "341.4mm")
+        new_weather(new_row, "Lowest temperature in °C", -40, 50, "-27.4°C")
+        new_weather(new_row, "Highest temperature in °C", -40, 50, "40.3°C")
+        if check_inputs(new_row, dont_send) == "no":
+            thank_you()
+            pass
+        else:
+            chart_question("rainfall")
+            time.sleep(2)
+            chart_question("contrasting temperatures")
+            time.sleep(2)
+            thank_you()
+
+
 main()
-    
-    
-    
-# Functions below for use later
-    
-    
-def new_worksheet_row():
-    """
-    Sends user input data to worksheet if it has been validated correct.
-    """
-    
-def sort_worksheet():
-    """
-    Sort worksheet to create a dataset in contrast to corresponding 
-    same day in previous years.
-    """
-    
-def display_chart():
-    """
-    Display a chart to the user comparing weather data with all 
-    past data on the same day of each year.
-    """
-
-
-
-
-    # """
-    # User input rainfall data for the day.
-    # """     
-    # while True:
-    #     print("Please enter the millimeters of rain today")
-    #     print("All numbers will be converted to decimals upto 1 decimal place")
-    #     print("Example input: 1.23456 Example output: 1.2")
-
-    #     try:
-    #         rain_num = float(input("Enter the amount of rain here:\n"))
-    #     except ValueError:
-    #         print(f"That wasn't a number. Please enter a number")
-    #         continue
-    #     else:
-    #         while rain_num not in range(0, 450): # Checks for excessive input value beyond expected amount.
-    #             print(f'You typed {rain_num} mm, this seems unusual: highest recorded UK rainfall in any 24hr period is 341.4mm')
-    #             print(f'Please try again.')
-    #             rain_num = float(input("Enter the amount of rain here:\n"))    
-    #         rain_num = round(rain_num, 1)
-    #         new_row_data.append(rain_num) # Add rainfall number to new row data list
-    #         print(new_row_data)                
-    #         break
-    # """
-    # User input minimum temperature data for the day.
-    # """
-    # while True:
-    #     print("Please enter the lowest temperature of rain today")
-    #     print("All numbers will be converted to decimals upto 1 decimal place")
-    #     print("Example input: 1.23456 Example output: 1.2")
-    #     try:
-    #         temp_min_num = float(input("Enter the lowest temperature here:\n"))
-    #     except ValueError:
-    #         print(f"That wasn't a number. Please enter a number")
-    #         continue
-    #     else:
-    #         while temp_min_num not in range(-40, 50): # Checks for excessive input value beyond expected amount.
-    #             print("You typed {temp_min_num} this seems unusual: coldest day on UK records = -27.2 'C")
-    #             print(f'Please try again.')
-    #             temp_min_num = float(input("Enter the lowest temperature here:\n"))    
-    #         temp_min_num = round(temp_min_num, 1)
-    #         new_row_data.append(temp_min_num) # Add min temperature number to new row data list
-    #         print(new_row_data)                
-    #         break
-    # """
-    # User input maximum temperature data for the day.
-    # """    
-    # while True:
-    #     print("Please enter the maximum temperature of rain today")
-    #     print("All numbers will be converted to decimals upto 1 decimal place")
-    #     print("Example input: 1.23456 Example output: 1.2")
-    #     try:
-    #         temp_max_num = float(input("Enter the highest temperature here:\n"))
-    #     except ValueError:
-    #         print(f"That wasn't a number. Please enter a number")
-    #         continue
-    #     else:
-    #         while temp_max_num not in range(-40, 50): # Checks for excessive input value beyond expected amount.
-    #             print("You typed {temp_max_num} this seems unusual: hottest day on UK records = 40.3 'C")
-    #             print(f'Please try again.')
-    #             temp_max_num = float(input("Enter the highest temperature here:\n"))    
-    #         temp_max_num = round(temp_max_num, 1)
-    #         new_row_data.append(temp_max_num) # Add max temperature number to new row data list
-    #         print(new_row_data)                
-    #         break
