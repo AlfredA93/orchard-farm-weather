f7130a61c396e02de82af15101a76a8b1189274e
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-07-03 10:01:18 +0200

    Edit plotext functions on find_rows()

--- test.py
+++ test.py
@@ -164,25 +164,25 @@ def find_rows(weather_type):
     day_of_year = int(datetime.now().strftime('%j')) 
     all_rows = DF.loc[DF['DOY'] == day_of_year] # Get all rows from column DOY (Day Of Year)
     years = all_rows["YEAR"].tolist()
+    rainfall = all_rows["RAINFALL"].tolist()
+    min_temp = all_rows["TEMP_MIN"].tolist()
+    max_temp = all_rows["TEMP_MAX"].tolist()
+    
+    plotext.title(f"Bar Chart of {weather_type.title()} since 1993")
+    plotext.xlabel("Year\n")
+    plotext.plotsize(120,40)
+    
     if weather_type == "rainfall":
-        col_name = "RAINFALL"
-        rainfall = all_rows[f"{col_name}"].tolist()
-        plotext.bar(years, rainfall, marker = "sd")
-        plotext.title(f"Bar Chart of {weather_type} of since 1993")
-        plotext.xlabel("Year\n")
-        plotext.ylabel(f"{weather_type} in mm")
-        plotext.plotsize(100,30)
-        plotext.show()
-        plotext.clear_data()
-    elif weather_type == "minimum temperature":
-        col_name = "TEMP_MIN"
-        min_temp = all_rows["TEMP_MIN"].tolist()
-        max_temp = all_rows["TEMP_MAX"].tolist()
-        plotext.multiple_bar(years, [min_temp, max_temp], label = ["min", "max"], width = 2 / 5)
-        plotext.plotsize(150,50)
-        plotext.show()
-    elif weather_type == "maximum temperature":
-        col_name = "TEMP_MAX"
+        plotext.bar(years, rainfall,  width = 2 / 5)
+        plotext.ylabel("Rainfall in mm")
+
+    elif weather_type == "contrasting temperatures":
+        plotext.multiple_bar(years, [max_temp, min_temp], label = ["max", "min"], width = 1 / 5)
+        plotext.ylabel("Temperature in °C")
+        plotext.xaxes(50)
+        
+    plotext.show()
+    plotext.clear_data()
     
     # years = all_rows["YEAR"].tolist() # Convert dataframe values to list
     # rainfall = all_rows[f"{col_name}"].tolist()
@@ -228,9 +228,7 @@ def main():
         else: 
             chart_question("rainfall")
             time.sleep(2)
-            chart_question("minimum temperature")
-            time.sleep(2)
-            chart_question("maximum temperature")
+            chart_question("contrasting temperatures")
             time.sleep(2)
             thank_you()
 main()
